7aa0b2aa0776b78b41704e2c38492107b900dc22
Mauricio Alvarez <maualvm@gmail.com>
2020-03-29 18:52:06 -0600

    Agregar display de imagen después de subida

--- Lab 12/controllerUpload.php
+++ Lab 12/controllerUpload.php
@@ -39,6 +39,9 @@
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            $image = $_FILES["fileToUpload"]["name"];
+            $img="uploads/".$image;
+            echo '<img src= "' . $img . '">';
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
